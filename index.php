0bf8581e7ecd66d0e1118301f581f1aef5927ed7
Rajdeep Singh <rsingh@bclc.com>
2018-04-03 10:54:15 -0700

    change 3

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <html><head><title><?php echo "containers at bclc";?></title></head>
 <body
-<?php 
-$image_url='/bclc-logo-960.jpg.scale.704x.jpg';
-?>
 
-<img src="<?php echo $image_url;?>">
+<?php
+    echo "<img src='/bclc-logo-960.jpg.scale.704x.jpg' alt='bclclogo' />";
+?>
 
 <br><h1>Openshift Container Platform at BCLC</h1>
