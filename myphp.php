f0751c223ab6fec60a150053c7a3abdb90e97955
Rajdeep Singh <rsingh@bclc.com>
2018-04-03 10:18:30 -0700

    added myphp.php

---
+++ myphp.php
@@ -0,0 +1,9 @@
+<html><head><title><?php echo "cool title";?></title></head>
+<body
+<?php 
+$image_url='http://corporate.bclc.com/content/dam/bclc/corporate/news-stories/bclc-logo-960.jpg.scale.704x.jpg';
+?>
+
+<img src="<?php echo $image_url;?>">
+
+<br><h1>Got me?</h1>
