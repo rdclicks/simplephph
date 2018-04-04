01998bd2b8f72f48d8ff8e304b9f116adb718ff3
Rajdeep Singh <rsingh@bclc.com>
2018-04-04 13:14:17 -0700

    1

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <html><head><title><?php echo "containers at bclc";?></title></head>
 <body
 
-<?php
-#phpinfo();
-echo '<img src="bclc-logo.jpg" alt="logo" />';
-
-?>
+<?php echo '<img src="bclc-logo.jpg" alt="logo" />';?>
 
 <br><h1>Openshift Container Platform at BCLC</h1>
