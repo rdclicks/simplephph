d1121d12f53b4f77f2d8e4e4b971d9550229960f
rdclicks <37884934+rdclicks@users.noreply.github.com>
2018-08-13 10:53:04 -0700

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<html><head><title><?php echo "containers at bclc";?></title></head>
-<body
-
-<?php echo '<img src="bclc-logo.jpg" alt="logo" />';?>
-
-<br><h1>Openshift Container Platform at BCLC</h1>
